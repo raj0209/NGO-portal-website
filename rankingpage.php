11d76704a0912a61b02c288d4198ba7f39586933
raj <buddhdev.raj34@gmail.com>
2014-04-01 17:05:33 +0530

    ranking & contact pages added

--- rankingpage.php
+++ rankingpage.php
@@ -1,8 +1,9 @@
 <?php
-	
 	include 'connect.php';
 	include 'database.php';
+
 	
+
 ?>	
 <!DOCTYPE html>
 <html>
@@ -55,8 +56,10 @@
 	  				<div class="nav-collapse collapse">
 	  					<ul class="nav nav-tabs">
 	  						<li><a class="brand" href="index.php">Sampark</a></li>
-	  						<li><a href="rankingpage.php">Top Organizations</a></li>
-	  						<li class="active"><a href="contact.php">Contact</a></li>
+	  						<!--<li class="active"><a href="#">Home</a></li>-->
+	  						<li class="active"><a href="rankingpage.php">Top Organizations</a></li>
+	  						<li><a href="contact.php">Contact</a></li>
+	  						
 	  					</ul>
 	  					<div align="center" class="nav-collapse collapse" style="margin:10px">
 	  						<ul class="nav">
@@ -78,7 +81,7 @@
 	  					</div>
 
 	  					<ul class="nav pull-right" style="margin-top:9px">
-							<input type="button" class="btn btn-success" name="signup" value="Create Account" id="signup" style="margin:10px" data-toggle="modal" data-target="#signupModal">
+							<input type="button" class="btn btn-success" name="signupButton" value="Create Account" id="signup" data-toggle="modal" style="margin:10px" data-target="#signupModal">	
 	  						<?php if(isset($_SESSION['SESS_MEMBER_ID'])) echo "<a style=\"color:white;text-decoration:none\" href=\"logout.php\"><button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\"style=\"margin:10px\" data-toggle=\"modal\">Log out</button></a>"; else echo "<button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\"style=\"margin:10px\" data-toggle=\"modal\" data-target=\"#signinModal\" id=\"signinHomeButton\">Sign In</button>" ?>
 	  					</ul>
 	  				</div><!--/.nav-collapse -->
@@ -247,125 +250,76 @@
 	  		</div>
 	  	</div><!-- /container -->
 
-
-
-
-	  	<div class="container">
-			<table class="table table-bordered">
-				<thead>
-				  <tr>
-					
-					<th class="span4">Team mentor name</th>
-					<th class="span4">Email</th>
-					<th class="span4">Contact Number</th>
-					<th>Webpage</th>
-				  </tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Prof. Pranav Joshi</td>
-						<td>pranavjoshi.gnr@gmail.com</td>
-						<td></td>
-						<td></td>
-					</tr>
-					<tr>
-						<td>Monish Dubey</td>
-						<td></td>
-						<td>+91-8128749949</td>
-						<td></td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-
-	  	<div class="container">
-			<table class="table table-bordered">
-				<thead>
-				  <tr>
-					
-					<th class="span4">Developing Team members</th>
-					<th class="span4">Email</th>
-					<th class="span4">Contact Number</th>
-					<th>Webpage</th>
-				  </tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Shubham Srivastava</td>
-						<td>shubham.s.del@gmail.com</td>
-						<td>+91-7874159310</td>
-						<td><a href="http://about.me/shubham.srivastava.101" target="_blank">http://about.me/shubham.srivastava.101</a></td>
-					</tr>
-					
-					<tr>
-						<td>Ronit Patel</td>
-						<td>ronitmutd.patel@gmail.com</td>
-						<td>+91-9825999972</td>
-						<td></td>
-					</tr>
-					
-					<tr>
-						<td>Vijay Dhameliya</td>
-						<td>vijay.atwork13@gmail.com</td>
-						<td>+91-8141006510</td>
-						<td></td>
-					</tr>+
-
-
-
-
-					
-					
-					<tr>
-						<td>Raj Buddhdev</td>
-						<td>buddhdev.raj34@gmail.com</td>
-						<td>+91-7359309891</td>
-						<td></td>
-					</tr>
-					
-					<tr>
-						<td>Sanket Jain</td>
-						<td>sanketjain5984@gmail.com</td>
-						<td>+91-9624264342</td>
-						<td></td>
-					</tr>
-					
-					<tr>
-						<td>Ronak Kothari</td>
-						<td>ronakkothari301@gmail.com</td>
-						<td>+91-7567249636</td>
-						<td></td>
-					</tr>
-					
-					<tr>
-						<td>Tushar Juneja</td>
-						<td>tusharj227@gmail.com</td>
-						<td>+91-7638153755</td>
-						<td></td>
-					</tr>
-					
-					<tr>
-						<td>Achint Mehta</td>
-						<td>achintjmehta93@gmail.com</td>
-						<td>+91-7405755993</td>
-						<td></td>
-					</tr>
-					
-					<tr>
-						<td>Rushik Patel</td>
-						<td>prushik6@gmail.com</td>
-						<td>+91-9898713101</td>
-						<td></td>
-					</tr>
-
-
-
-				</tbody>
-			</table>
-		</div>
-
 	 
-		
+		<div class="container">
+			<table class="table table-bordered">
+				<thead>
+				  <tr>
+					<th>Rank</th>
+					<th>Name of the Organisation</th>
+					<th>Location</th>
+					<th>Contact Number</th>
+					<th>Rating</th>
+				  </tr>
+				</thead>
+				<tbody>
+					
+					<?php
+						$result=mysql_query("SELECT * FROM Ngo Order by weighted_rate DESC");
+						$cnt=1;
+						while($cnt<=12)
+						{
+							$row = mysql_fetch_array($result);
+							?>
+						<tr>
+							<td><?php echo $cnt ?></td>
+							<td><?php echo $row['name'];?></td>
+							<td><?php echo $row['address'];?></td>
+							<td><?php echo $row['contact'];?></td>
+							<td><?php echo $row['weighted_rate'];?></td>
+
+						</tr>
+
+						<?php $cnt=$cnt+1;
+					}			
+					?> <!--
+						$_name=mysql_query("SELECT name from Ngo ORDER BY weighted_rating DESC");
+						$_location=mysql_query("SELECT location from Ngo ORDER BY weighted_rating DESC");
+						$_domain=mysql_query("SELECT catNgo.category from Ngo, catNgo where catNgo.Ngo_pid=Ngo.Ngo_pid ORDER BY Ngo.weighted_rating DESC");
+						$_rating=mysql_query("SELECT weighted_rating from Ngo ORDER BY weighted_rating DESC");
+						
+
+						$i;
+
+						for($i=0;$i<12;$i++)
+							print "<tr><td>$_name[i]</td><td>$_location[i]</td><td>$_domain[i]</td><td>$_rating[i]</td></tr>\n"
+
+						<td>1</td>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+						<td>1</td>
+					</tr>
+					
+					<tr>
+						<td>2</td>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
+						<td>1</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>Larry the Bird</td>
+						<td>@twitter</td>
+						<td>1</td>
+						<td>1</td>
+					</tr>-->
+
+
+				</tbody>
+			</table>
+		</div>
 	</body>
 
 
