50b44ba37ff679142bae2c8dd56d303c78db3f4f
Vijay Dhameliya <vijay.atwork13@gmail.com>
2014-04-04 12:15:11 +0530

    Moved slider images into img

--- index.php
+++ index.php
@@ -75,33 +75,33 @@ include 'database.php';
 		<div class="backimage" style="margin-left:20px; margin-right:15px;">
 			<div id="wowslider-container1" style="margin-top:40px;">
 				<div class="ws_images"><ul>
-					<li><img src="data1/images/pic102.jpg" alt="pic102" title="" id="wows1_0"/></li>
-					<li><img src="data1/images/pic103.jpg" alt="pic103" title="" id="wows1_1"/></li>
-					<li><img src="data1/images/pic104.jpg" alt="pic104" title="" id="wows1_2"/></li>
-					<li><img src="data1/images/pic105.jpg" alt="pic105" title="" id="wows1_3"/></li>
-					<li><img src="data1/images/pic106.jpg" alt="pic106" title="" id="wows1_4"/></li>
-					<li><img src="data1/images/pic107.jpg" alt="pic107" title="" id="wows1_5"/></li>
-					<li><img src="data1/images/pic108.jpg" alt="pic108" title="" id="wows1_6"/></li>
-					<li><img src="data1/images/coname.jpg" alt="co-name" title="" id="wows1_7"/></li>
+					<li><img src="img/slider_images/images/pic102.jpg" alt="pic102" title="" id="wows1_0"/></li>
+					<li><img src="img/slider_images/images/pic103.jpg" alt="pic103" title="" id="wows1_1"/></li>
+					<li><img src="img/slider_images/images/pic104.jpg" alt="pic104" title="" id="wows1_2"/></li>
+					<li><img src="img/slider_images/images/pic105.jpg" alt="pic105" title="" id="wows1_3"/></li>
+					<li><img src="img/slider_images/images/pic106.jpg" alt="pic106" title="" id="wows1_4"/></li>
+					<li><img src="img/slider_images/images/pic107.jpg" alt="pic107" title="" id="wows1_5"/></li>
+					<li><img src="img/slider_images/images/pic108.jpg" alt="pic108" title="" id="wows1_6"/></li>
+					<li><img src="img/slider_images/images/coname.jpg" alt="co-name" title="" id="wows1_7"/></li>
 					</ul>
 				</div>
 				<div class="ws_bullets"><div>
-					<a href="#" title="pic102"><img src="data1/tooltips/pic102.jpg" alt="pic102"/>1</a>
-					<a href="#" title="pic103"><img src="data1/tooltips/pic103.jpg" alt="pic103"/>2</a>
-					<a href="#" title="pic104"><img src="data1/tooltips/pic104.jpg" alt="pic104"/>3</a>
-					<a href="#" title="pic105"><img src="data1/tooltips/pic105.jpg" alt="pic105"/>4</a>
-					<a href="#" title="pic106"><img src="data1/tooltips/pic106.jpg" alt="pic106"/>5</a>
-					<a href="#" title="pic107"><img src="data1/tooltips/pic107.jpg" alt="pic107"/>6</a>
-					<a href="#" title="pic108"><img src="data1/tooltips/pic108.jpg" alt="pic108"/>7</a>
-					<a href="#" title="co-name"><img src="data1/tooltips/coname.jpg" alt="co-name"/>8</a>
+					<a href="#" title="pic102"><img src="img/slider_images/tooltips/pic102.jpg" alt="pic102"/>1</a>
+					<a href="#" title="pic103"><img src="img/slider_images/tooltips/pic103.jpg" alt="pic103"/>2</a>
+					<a href="#" title="pic104"><img src="img/slider_images/tooltips/pic104.jpg" alt="pic104"/>3</a>
+					<a href="#" title="pic105"><img src="img/slider_images/tooltips/pic105.jpg" alt="pic105"/>4</a>
+					<a href="#" title="pic106"><img src="img/slider_images/tooltips/pic106.jpg" alt="pic106"/>5</a>
+					<a href="#" title="pic107"><img src="img/slider_images/tooltips/pic107.jpg" alt="pic107"/>6</a>
+					<a href="#" title="pic108"><img src="img/slider_images/tooltips/pic108.jpg" alt="pic108"/>7</a>
+					<a href="#" title="co-name"><img src="img/slider_images/tooltips/coname.jpg" alt="co-name"/>8</a>
 					</div>
 				</div>
 				
 				<a href="#" class="ws_frame"></a>
 					<div class="ws_shadow"></div>
 					</div>
-						<script type="text/javascript" src="engine1/wowslider.js"></script>
-						<script type="text/javascript" src="engine1/script.js"></script>
+						<script type="text/javascript" src="engine/wowslider.js"></script>
+						<script type="text/javascript" src="engine/script.js"></script>
 				<div >
 					
 					<div class="DetailBoxes">
